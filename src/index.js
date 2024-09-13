cbf653a5979fbb27b19dc623fcd743b32e27ab0a
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-09-13 09:33:59 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,44 +1,60 @@
-import express from 'express';
+import { startServer } from './server.js';
 
-const app = express();
+startServer();
 
-const PORT = 3000;
+// import express from 'express';
+// import pino from 'pino-http';
+// import cors from 'cors';
 
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
+// const app = express();
 
-app.use(express.json());
-4;
+// const PORT = 3000;
 
-app.get('/', (req, res, next) => {
-  res.json({
-    message: 'Hello m@loy',
-  });
-});
+// app.use(cors());
 
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Не підтримується',
-  });
-});
+// app.use(
+//   pino({
+//     transport: {
+//       target: 'pino-pretty',
+//     },
+//   }),
+// );
 
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'не існує',
-  });
-});
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString()}`);
+//   next();
+// });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Не існує такого контенту',
-    error: err.message,
-  });
-});
+// app.use(express.json());
+// 4;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.get('/', (req, res, next) => {
+//   res.json({
+//     message: 'Hello m@loy',
+//   });
+// });
 
-// http://localhost:3000
+// app.use('*', (req, res, next) => {
+//   res.status(404).json({
+//     message: 'Не підтримується',
+//   });
+// });
+
+// app.use('*', (req, res, next) => {
+//   res.status(404).json({
+//     message: 'не існує',
+//   });
+// });
+
+// app.use((err, req, res, next) => {
+//   res.status(500).json({
+//     message: 'Не існує такого контенту',
+//     error: err.message,
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+// // http://localhost:3000
